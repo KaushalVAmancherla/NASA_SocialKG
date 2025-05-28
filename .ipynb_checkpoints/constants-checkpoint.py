6eae44a4ba9af483f507a346c626cb7a16d6e539
kaushalamancherla <amancherlakaushal@gmail.com>
2025-05-28 10:45:35 -0700

    Finished retrieving all raw data from OpenAlex

--- .ipynb_checkpoints/constants-checkpoint.py
+++ .ipynb_checkpoints/constants-checkpoint.py
@@ -7,6 +7,8 @@ JPL_individual_fp = jpl_data_dir + 'OpenAlex_JPL_individuals.csv'
 
 person_raw_openalex_data_fp = jpl_data_dir + 'person_open_alex_raw.json'
 inst_raw_openalex_data_fp = jpl_data_dir + 'inst_open_alex_raw.json'
+works_raw_openalex_data_fp = jpl_data_dir + 'works_open_alex_raw.json'
+
 person_to_works_id_jpl_fp = jpl_data_dir + 'person_to_works_id.json'
 
 ontology_fp = './ontologies/Social_KG.rdf'
